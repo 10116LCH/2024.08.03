79edec5791e73af939fe26686ffcc3a9f63b3de3
User <leechaehoon264@gmail.com>
2024-08-24 10:53:03 +0900

    변수 입출력 변수 데이터 타입, 형변환 변수 call by value, call by reference 차이

--- practice/second.py
+++ practice/second.py
@@ -8,12 +8,21 @@ print(a)
 b = input("값을 넣으시오 : ")
 print(b + "(이)가 입력되었습니다.")
 
-c = int(b) + 158
+b = "158"
+c = int(b) + 151
 print(c)
 
 d = c
 print("c의 값은", c)
 print("d의 값은", d)
-c = 100
+c = 610
 print("c의 값은", c)
 print("d의 값은", d)
+
+e = [100, 96, 87, 100]
+f = e
+print(e)
+print(f)
+e.append(95)
+print(e)
+print(f)
