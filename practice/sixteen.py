faafadbd06238a3a1f2293558d965e1a65d85118
10116LCH <leechaehoon264@gmail.com>
2025-02-01 11:08:46 +0900

    Add files via upload

--- practice/sixteen.py
+++ practice/sixteen.py
@@ -8,11 +8,13 @@ print(d.keys())
 print(d.values())
 print("-----------------------------")
 
-inventory = {}
+inventory = {"물":"1000", "콜라":"1200", "사이다":"1100", "커피":"500"}
 #사용자 모드
-#없으면 x
-#뭔가 있으면 o
-#해당사항 없으면 x
+#음료는 물, 콜라, 사이다, 커피
+#가격은 1000, 1200, 1100, 500
+#질문 대답
+#해당음료가 있으면 o
+#해당음료가 없으면 x
 if not inventory :
     print("X")
 elif inventory :
@@ -23,7 +25,9 @@ elif inventory :
         print("X")
 
 #관리자 모드
-#입력 가능
+#원하는 음료와 가격 작성
+#만약 기존에 있다면 가격 변동
+#없다면 새로 추가
 c = input("입력")
 if c in inventory:
     print()
